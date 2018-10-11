6242e338a81140a7691ef2f82a9c3d88cee02593
Charlie von Lehe <vonlehec@gmail.com>
2018-10-11 16:00:54 -0400

    Added github code

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 var port = process.env.PORT || 3000;
 http.createServer(function (req, res) {
-  res.writeHead(200);
+  res.writeHead(200, {'Content-Type': 'application/json'});
   res.end();
 }).listen(port);
