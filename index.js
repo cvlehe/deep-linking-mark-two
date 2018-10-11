8d280e9ecacfcb2150276628d332a27df27a2637
Charlie von Lehe <vonlehec@gmail.com>
2018-10-11 15:39:57 -0400

    Added aasa file to well-know Removed json code from index.js

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
 var http = require('http');
-var json = {
-  "applinks": {
-    "apps": [],
-    "details": [
-      { 
-        "appID": "P4LZFPM2NH.org.reactjs.native.example.deeplinkingmarktwo",
-        "paths": [ "/mobile/*" ]
-      }
-    ]
-  }
-};
+
 var port = process.env.PORT || 3000;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'application/json'});
-  res.end(JSON.stringify(json));
+  res.end();
 }).listen(port);
